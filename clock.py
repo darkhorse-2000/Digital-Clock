774cb464691ef5c42458e869abf42a709c41afad
darkhorse-2000 <72261729+darkhorse-2000@users.noreply.github.com>
2021-06-23 19:41:37 +0530

    Update clock.py

--- clock.py
+++ clock.py
@@ -1,6 +1,6 @@
 from tkinter import *
-from tkinter.messagebox import showinfo
-from tkinter.filedialog import askopenfilename, asksaveasfilename
+# from tkinter.messagebox import showinfo
+# from tkinter.filedialog import askopenfilename, asksaveasfilename
 import time
 
 root = Tk()
@@ -13,26 +13,29 @@ def clock():
     minute = time.strftime("%M")
     second = time.strftime("%S")
     day = time.strftime("%A")
-    am_pm = time.strftime("%p")
+    AM_PM = time.strftime("%p")
     time_zone = time.strftime("%Z")
 
-    my_label.config(text=hour + ":" + minute +  ":" + second + " " + am_pm)
-    my_label.after(1000, clock)
+    my_label1.config(text=hour + ":" + minute +  ":" + second + " " + AM_PM)
+    my_label1.after(1000, clock)
 
     my_label2.config(text=time_zone + " " + day)
 
 def update():
-    my_label.config(text="New Text")
+    my_label1.config(text="New Text")
 
-my_label = Label(root,text="", font=("Helvetica", 48), fg="green", bg="black")
-my_label.pack(pady=20)
+my_label1 = Label(root,text="", font=("Helvetica", 48), fg="green", bg="black")
+my_label1.pack(pady=20)
 
 my_label2 = Label(root,text="", font=("Helvetica", 14))
 my_label2.pack(pady=10)
 
-clock()
+clock() # Calling the clock function
 # my_label.after(5000, update)
 
 
 
 root.mainloop()
+
+
+"""SS"""
