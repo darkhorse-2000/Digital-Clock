343e393c86fa13bc669a3d6e1df02a06938028ae
darkhorse-2000 <72261729+darkhorse-2000@users.noreply.github.com>
2021-06-23 19:38:32 +0530

    Add files via upload

---
+++ clock.py
@@ -0,0 +1,38 @@
+from tkinter import *
+from tkinter.messagebox import showinfo
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+import time
+
+root = Tk()
+root.geometry("655x444")
+root.title("Tik-Tok-Tik-Tok")
+root.wm_iconbitmap("4.ico")
+
+def clock():
+    hour = time.strftime("%I")
+    minute = time.strftime("%M")
+    second = time.strftime("%S")
+    day = time.strftime("%A")
+    am_pm = time.strftime("%p")
+    time_zone = time.strftime("%Z")
+
+    my_label.config(text=hour + ":" + minute +  ":" + second + " " + am_pm)
+    my_label.after(1000, clock)
+
+    my_label2.config(text=time_zone + " " + day)
+
+def update():
+    my_label.config(text="New Text")
+
+my_label = Label(root,text="", font=("Helvetica", 48), fg="green", bg="black")
+my_label.pack(pady=20)
+
+my_label2 = Label(root,text="", font=("Helvetica", 14))
+my_label2.pack(pady=10)
+
+clock()
+# my_label.after(5000, update)
+
+
+
+root.mainloop()
